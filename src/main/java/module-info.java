60641c8258d9e67fb50a9994c27769f42e6eea1f
BUDA124 <castillocarlosccm.421@gmai.com>
2025-10-18 18:16:33 -0600

    Mi primer commit: subiendo el proyecto inicial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module tec.curso.prototype {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens tec.curso.prototype to javafx.fxml;
+    exports tec.curso.prototype;
+}
