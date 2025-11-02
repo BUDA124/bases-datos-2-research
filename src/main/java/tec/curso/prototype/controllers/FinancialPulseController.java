9a9f90e69fb7f4df26e8801155fb2d8da46f748d
BUDA124 <castillocarlosccm.421@gmai.com>
2025-11-01 19:15:34 -0600

    Update to screens

--- src/main/java/tec/curso/prototype/controllers/FinancialPulseController.java
+++ src/main/java/tec/curso/prototype/controllers/FinancialPulseController.java
@@ -1,9 +1,15 @@
 package tec.curso.prototype.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import tec.curso.prototype.JavaFxApplication;
@@ -24,9 +30,28 @@ public class FinancialPulseController {
         // actualizarDatos();
     }
 
+    /**
+     * This method is now designed to be called by a button's onAction event.
+     * It simulates registering a sale and then shows a confirmation notification.
+     *
+     * @param event The ActionEvent triggered by the UI component (e.g., a button click).
+     */
     @FXML
-    private void registerSale() {
-        // TODO
+    private void registerSale(ActionEvent event) {
+        // TODO: Add the business logic here to save the sale data.
+
+        // Example:
+        // String product = productTextField.getText();
+        // double amount = Double.parseDouble(amountTextField.getText());
+        // financialService.saveSale(product, amount);
+
+        Notifications.create()
+                .title("Sale Registered")
+                .text("The new sale has been added successfully.")
+                .position(Pos.BOTTOM_LEFT)
+                .hideAfter(Duration.seconds(5)) // Auto-hide after 5 seconds
+                .owner(getStageFromEvent(event)) // Attach to the correct window
+                .showConfirm(); // Display with a confirmation icon
     }
 
     private void actualizarDatos() {
@@ -36,5 +61,9 @@ public class FinancialPulseController {
     @FXML
     private void action() throws IOException {
         JavaFxApplication.changeScene("SingUp.fxml");
+    }
+
+    private Stage getStageFromEvent(ActionEvent event) {
+        return (Stage) ((Node) event.getSource()).getScene().getWindow();
     }
 }
